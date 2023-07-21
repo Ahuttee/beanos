b1f96b0cac764f74605bb634e58a057dc3a4f20f
Ahuttee <Ahuttee595@gmail.com>
2023-07-21 20:33:32 +0500

    Convert tabs to spaces

--- cogs/Misc.py
+++ cogs/Misc.py
@@ -5,141 +5,141 @@ import random
 import aiohttp
 
 with open("dictionary.txt", 'r') as f:
-	dictionary = f.read().split('\n')
+    dictionary = f.read().split('\n')
 
 
 class ImageView(discord.ui.View):
-	def __init__(self, embed, site_url):
-		super().__init__()
-		self.embed = embed
-		self.site_url = site_url
-		self.timeout = 120.0
-	
-	@discord.ui.button(label="NEW", style=discord.ButtonStyle.green)
-	async def new(self, inter: discord.Interaction, button: discord.ui.Button):
-		async with aiohttp.ClientSession() as session:
-			async with session.get(self.site_url) as response:
-				if response.status == 200:
-					data = await response.json()
-					await self.new_image(inter, data)
-				else:
-					await inter.response.send_message("Unable to get a new picture", ephemeral=True)
-					
-	async def new_image(self, inter, data):
-		pass
-		
-	async def on_timeout(self):
-		self.clear_items()
-		await self.message.edit(view=self)
-			
-			
+    def __init__(self, embed, site_url):
+        super().__init__()
+        self.embed = embed
+        self.site_url = site_url
+        self.timeout = 120.0
+    
+    @discord.ui.button(label="NEW", style=discord.ButtonStyle.green)
+    async def new(self, inter: discord.Interaction, button: discord.ui.Button):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.site_url) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    await self.new_image(inter, data)
+                else:
+                    await inter.response.send_message("Unable to get a new picture", ephemeral=True)
+                    
+    async def new_image(self, inter, data):
+        pass
+        
+    async def on_timeout(self):
+        self.clear_items()
+        await self.message.edit(view=self)
+            
+            
 
 class CatView(ImageView):
-	def __init__(self, embed, site_url):
-		super().__init__(embed, site_url)
-		
-	async def new_image(self, inter, data):
-		url = data[0]["url"]
-		self.embed.set_image(url=url)
-		await inter.response.edit_message(embed=self.embed, view=self)
-		
+    def __init__(self, embed, site_url):
+        super().__init__(embed, site_url)
+        
+    async def new_image(self, inter, data):
+        url = data[0]["url"]
+        self.embed.set_image(url=url)
+        await inter.response.edit_message(embed=self.embed, view=self)
+        
 
 class DogView(ImageView):
-	def __init__(self, embed, site_url):
-		super().__init__(embed, site_url)
-		
-	async def new_image(self, inter, data):
-		url = data["message"]
-		self.embed.set_image(url=url)
-		await inter.response.edit_message(embed=self.embed, view=self)
-	
-					
+    def __init__(self, embed, site_url):
+        super().__init__(embed, site_url)
+        
+    async def new_image(self, inter, data):
+        url = data["message"]
+        self.embed.set_image(url=url)
+        await inter.response.edit_message(embed=self.embed, view=self)
+    
+                    
 class Misc(commands.Cog):
-	def __init__(self, bot):
-		self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-		self.view_avatar_ctx_menu = app_commands.ContextMenu(
-			name = "View avatar",
-			callback=self.view_avatar
-		)
+        self.view_avatar_ctx_menu = app_commands.ContextMenu(
+            name = "View avatar",
+            callback=self.view_avatar
+        )
 
-		self.bot.tree.add_command(self.view_avatar_ctx_menu)
-
-
-	async def view_avatar(self, inter: discord.Interaction, user: discord.User):
-		embed = discord.Embed(
-			title = "Avatar",
-			color = discord.Colour.from_rgb(40, 1, 92)
-		)
-		embed.set_image(url=user.avatar.url)
-		embed.set_footer(text = user.name + "#" + user.discriminator, icon_url = user.avatar.url)
-
-		await inter.response.send_message(embed=embed)
-
-	@commands.hybrid_command(name="godspeak")
-	async def godspeak(self, ctx, number: int):
-		if number < 150:
-			await ctx.send(' '.join([random.choice(dictionary) for _ in range(number)]))
-		else:
-			await ctx.send("Too large, use a value of less than 150", ephemeral=True)
+        self.bot.tree.add_command(self.view_avatar_ctx_menu)
 
 
-	@commands.hybrid_command(name="cat", aliases=['neko'])
-	async def cat(self, ctx):
-		site_url = "https://api.thecatapi.com/v1/images/search"
-		async with aiohttp.ClientSession() as session:
-			async with session.get(site_url) as response:
-				if response.status == 200:
-					data = await response.json()
-					url = data[0]["url"]
-				else:
-					await ctx.send("Unable to get an image, try again later", ephemeral=True)
-					return
+    async def view_avatar(self, inter: discord.Interaction, user: discord.User):
+        embed = discord.Embed(
+            title = "Avatar",
+            color = discord.Colour.from_rgb(40, 1, 92)
+        )
+        embed.set_image(url=user.avatar.url)
+        embed.set_footer(text = user.name + "#" + user.discriminator, icon_url = user.avatar.url)
 
-		embed = discord.Embed(
-			title = "c a t",
-			color = discord.Colour.random()
-		)
-		embed.set_footer(text = "www.thecatapi.com")
-		embed.set_image(url=url)
-		
-		view = CatView(embed, site_url)
-		view.message = await ctx.send(embed=embed, view=view)
+        await inter.response.send_message(embed=embed)
 
-		#await view.wait()
-		#await msg.edit(embed=embed, view=None)
-		
-		
-	@commands.hybrid_command(name="dog", aliases=['doge'])
-	async def dog(self, ctx):
-		site_url = "https://dog.ceo/api/breeds/image/random"
-		async with aiohttp.ClientSession() as session:
-			async with session.get(site_url) as response:
-				if response.status == 200:
-					data = await response.json()
-					url = data["message"]
-				else:
-					await ctx.send("Unable to get an image, try again later", ephemeral=True)
-					return
-
-		embed = discord.Embed(
-			title = "doge",
-			color = discord.Colour.random()
-		)
-		embed.set_footer(text = "www.dog.ceo")
-		embed.set_image(url=url)
-		
-		view = DogView(embed, site_url)
-		view.message = await ctx.send(embed=embed, view=view)
-
-	#	await view.wait()
-	#	await msg.edit(embed=embed, view=None)
-			
+    @commands.hybrid_command(name="godspeak")
+    async def godspeak(self, ctx, number: int):
+        if number < 150:
+            await ctx.send(' '.join([random.choice(dictionary) for _ in range(number)]))
+        else:
+            await ctx.send("Too large, use a value of less than 150", ephemeral=True)
 
 
-		
+    @commands.hybrid_command(name="cat", aliases=['neko'])
+    async def cat(self, ctx):
+        site_url = "https://api.thecatapi.com/v1/images/search"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(site_url) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    url = data[0]["url"]
+                else:
+                    await ctx.send("Unable to get an image, try again later", ephemeral=True)
+                    return
+
+        embed = discord.Embed(
+            title = "c a t",
+            color = discord.Colour.random()
+        )
+        embed.set_footer(text = "www.thecatapi.com")
+        embed.set_image(url=url)
+        
+        view = CatView(embed, site_url)
+        view.message = await ctx.send(embed=embed, view=view)
+
+        #await view.wait()
+        #await msg.edit(embed=embed, view=None)
+        
+        
+    @commands.hybrid_command(name="dog", aliases=['doge'])
+    async def dog(self, ctx):
+        site_url = "https://dog.ceo/api/breeds/image/random"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(site_url) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    url = data["message"]
+                else:
+                    await ctx.send("Unable to get an image, try again later", ephemeral=True)
+                    return
+
+        embed = discord.Embed(
+            title = "doge",
+            color = discord.Colour.random()
+        )
+        embed.set_footer(text = "www.dog.ceo")
+        embed.set_image(url=url)
+        
+        view = DogView(embed, site_url)
+        view.message = await ctx.send(embed=embed, view=view)
+
+    #   await view.wait()
+    #   await msg.edit(embed=embed, view=None)
+            
+
+
+        
 
 async def setup(bot):
-	await bot.add_cog(Misc(bot))
+    await bot.add_cog(Misc(bot))
 
-	
+    
--- main.py
+++ main.py
@@ -9,38 +9,38 @@ log_handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode
 
 # For heroku server
 if 'HEROKU' in os.environ:
-	config = {
-		"prefix": os.environ['PREFIX'],
-		"token": os.environ['TOKEN']
-	}
+    config = {
+        "prefix": os.environ['PREFIX'],
+        "token": os.environ['TOKEN']
+    }
 else:
-	with open("config.json", 'r') as file:
-		config = json.load(file)
+    with open("config.json", 'r') as file:
+        config = json.load(file)
 
 intents = discord.Intents.default()
 
 bot = commands.Bot(
-	command_prefix = config['prefix'],
-	intents=intents
+    command_prefix = config['prefix'],
+    intents=intents
 )
 
 @bot.event
 async def on_ready():
-	await bot.tree.sync()
+    await bot.tree.sync()
 
-	activity = discord.Streaming(
-		name = "",
-		url = "https://www.twitch.tv/quandaledingle1235"
-	)
-	await bot.change_presence(activity=activity)
-	print("Bot is online")
+    activity = discord.Streaming(
+        name = "",
+        url = "https://www.twitch.tv/quandaledingle1235"
+    )
+    await bot.change_presence(activity=activity)
+    print("Bot is online")
 
 async def load_cogs():
-	for file in os.listdir('./cogs'):
-		if file.endswith(".py"):
-			extension = file[:-3]
-			await bot.load_extension(f"cogs.{extension}")
-			print("Loaded extension", extension)
+    for file in os.listdir('./cogs'):
+        if file.endswith(".py"):
+            extension = file[:-3]
+            await bot.load_extension(f"cogs.{extension}")
+            print("Loaded extension", extension)
 
 asyncio.run(load_cogs())
-bot.run(config['token'], log_handler=log_handler, log_level=logging.DEBUG)
+bot.run(config['token'], log_handler=log_handler, log_level=logging.WARNING)
