eb4ae1619f620eb4e5fc4cc8aced64e0d73f9184
Ahuttee <Ahuttee595@gmail.com>
2023-07-22 19:58:10 +0500

    Change activity status every 5 mins

--- main.py
+++ main.py
@@ -1,46 +1,54 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import json
 import asyncio
 import os
 import logging
+from itertools import cycle
+
+
+status_msg = cycle( ["with Shaq", "with Kai Cenat", "with Quandale Dingle", "with you"] )
 
 log_handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
-# For heroku server
-if 'HEROKU' in os.environ:
-    config = {
-        "prefix": os.environ['PREFIX'],
-        "token": os.environ['TOKEN']
-    }
-else:
-    with open("config.json", 'r') as file:
-        config = json.load(file)
-
 intents = discord.Intents.default()
 
+with open("config.json", 'r') as file:
+        config = json.load(file)
+
 bot = commands.Bot(
-    command_prefix = config['prefix'],
-    intents=intents
+	command_prefix = config['prefix'],
+	intents=intents
 )
 
 @bot.event
 async def on_ready():
-    await bot.tree.sync()
+	await bot.tree.sync()
+	print("Bot is online")
 
+@tasks.loop(minutes=5)
+async def update_status():
     activity = discord.Streaming(
-        name = "",
-        url = "https://www.twitch.tv/quandaledingle1235"
-    )
+		name = next(status_msg),
+		url = "https://www.twitch.tv/quandaledingle1235"
+	)
     await bot.change_presence(activity=activity)
-    print("Bot is online")
+    
 
 async def load_cogs():
-    for file in os.listdir('./cogs'):
-        if file.endswith(".py"):
-            extension = file[:-3]
-            await bot.load_extension(f"cogs.{extension}")
-            print("Loaded extension", extension)
+	for file in os.listdir('./cogs'):
+		if file.endswith(".py"):
+			extension = file[:-3]
+			await bot.load_extension(f"cogs.{extension}")
+			print("Loaded extension", extension)
 
 asyncio.run(load_cogs())
-bot.run(config['token'], log_handler=log_handler, log_level=logging.WARNING)
+
+
+# For hosting on Replit
+if "REPLIT" in os.environ:
+  import keep_alive
+  keep_alive.main()
+  config["token"] = os.environ["TOKEN"]
+  
+bot.run(config["token"], log_handler=log_handler, log_level=logging.WARNING)
