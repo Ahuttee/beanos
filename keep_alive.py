ed4529a84f3e818e63ca8149784b5812bf12b728
Ahuttee <Ahuttee595@gmail.com>
2023-07-23 14:12:51 +0000

    Add our own code for pinging the replit site for keeping the bot online

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,28 @@
 from flask import Flask
 from threading import Thread
+import os
+import requests
+import time
 
-app = Flask("beanos")
+app = Flask('Beanos')
 
-@app.route("/")
-def index():
-  return "Hello world :)"
-
+@app.route('/')
+def home():
+    return "Hello world! :)"
 def run():
-  app.run(host="0.0.0.0", port=8080)
+  app.run(host='0.0.0.0',port=8080)
 
-def main():
-  t = Thread(target=run)
-  t.start()
+def ping():
+  while True:
+    url = os.environ["URL"]
+    status = requests.get(url)
+    print("Status", status)
+    time.sleep(180)
+  
+
+
+def main():  
+    server = Thread(target=run)
+    pinger = Thread(target=ping)
+    server.start()
+    pinger.start()
